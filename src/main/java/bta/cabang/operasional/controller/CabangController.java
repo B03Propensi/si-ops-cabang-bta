dd148ea83c4f1eee60d0ecce497b8d4e643a94c5
Muhammad Farras Nur Aslam <mfarrasna@gmail.com>
2021-04-27 02:10:16 +0700

    add controller and list cabang

--- src/main/java/bta/cabang/operasional/controller/CabangController.java
+++ src/main/java/bta/cabang/operasional/controller/CabangController.java
@@ -1,10 +1,17 @@
 package bta.cabang.operasional.controller;
 
+import bta.cabang.operasional.model.CabangModel;
 import bta.cabang.operasional.repository.UserDb;
+import bta.cabang.operasional.security.AuthService;
 import bta.cabang.operasional.service.CabangService;
+import bta.cabang.operasional.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
 
 @Controller
 @RequestMapping("/cabang")
@@ -13,7 +20,18 @@ public class CabangController {
     private CabangService cabangService;
 
     @Autowired
-    private  UserDb userDb;
+    private UserService userService;
+
+    @Autowired
+    private AuthService authService;
+
+    @GetMapping("")
+    private String viewallcabang (Model model){
+        List<CabangModel> cabangList = cabangService.getCabangList();
+        model.addAttribute("cabangList", cabangList);
+        return "view-all-cabang";
+    }
+
 
 
 
