d3b7ab3929d4100a9610cf905d55063cbd2903f2
Muhammad Farras Nur Aslam <mfarrasna@gmail.com>
2021-05-25 14:53:49 +0700

    usermodel

--- src/main/java/bta/cabang/operasional/model/UserModel.java
+++ src/main/java/bta/cabang/operasional/model/UserModel.java
@@ -14,7 +14,7 @@ import java.util.Date;
 import java.util.List;
 
 @Entity
-@Table(name = "\"pengguna\"")
+@Table(name = "\"User\"")
 public class UserModel implements Serializable {
     @Id
     @Column(name = "id_user")
